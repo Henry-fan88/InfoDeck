ad280e24a1c2c6a7274f3adf1f3607a0aa877b46
Henry-fan88 <fanleheng@gmail.com>
2026-03-11 08:55:06 +0800

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -197,7 +197,7 @@ export default function App() {
       },
       {
         id: 'finance',
-        name: 'Financial',
+        name: 'Finance',
         icon: 'TrendingUp',
         color: 'violet',
         feeds: [
